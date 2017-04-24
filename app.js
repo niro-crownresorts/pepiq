c5adc073a46bb26fe81135b4dd6fda8c2312c0f8
Niroshana Pathirana <niro.crownresorts@gmail.com>
2017-04-24 21:29:12 +1000

    Replaced index.js with app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+var Flint = require('node-flint');
+var webhook = require('node-flint/webhook');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+
+// flint options
+var config = {
+  webhookUrl: 'https://niro-spark-bot.herokuapp.com/flint',
+  token: 'NTFkYjAyMDgtNTdkZi00YjQ5LWE5OTItNzY2Mzk5MGUzYmIyNDJlNGRmNDItMDJk',
+  port: 8080,
+  removeWebhooksOnStart: false,
+  maxConcurrent: 5,
+  minTime: 50
+};
+
+// init flint
+var flint = new Flint(config);
+flint.start();
+console.log("Starting flint, please wait...");
+
+flint.on("initialized", function() {
+  console.log("Flint initialized successfully! [Press CTRL-C to quit]");
+});
+
+// say hello
+flint.hears('/hello', function(bot, trigger) {
+  bot.say('Hello %s!', trigger.personDisplayName);
+});
+
+// add flint event listeners
+flint.on('message', function(bot, trigger, id) {
+  flint.debug('"%s" said "%s" in room "%s"', trigger.personEmail, trigger.text, trigger.roomTitle);
+});
+
+flint.on('initialized', function() {
+  flint.debug('initialized %s rooms', flint.bots.length);
+});
+
+// define express path for incoming webhooks
+app.post('/flint', webhook(flint));
+
+// start express server
+var port = process.env.PORT || config.port;
+
+var server = app.listen(port, function () {
+  flint.debug('Flint listening on port %s', port);
+});
+
+// gracefully shutdown (ctrl-c)
+process.on('SIGINT', function() {
+  flint.debug('stoppping...');
+  server.close();
+  flint.stop().then(function() {
+    process.exit();
+  });
+});
